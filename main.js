cf8cda5d67bd551d35890c83f4ee57f6ac045d14
pakkofrank <jpalma@ucol.mx>
2019-09-18 23:30:26 -0500

    valores de las casillas puesto

--- main.js
+++ main.js
@@ -25,38 +25,138 @@ class Jugador1 extends Jugadores {
         let n = this._dado.lanzar();
         this._posicion += n;
 
+        //escaleras
+
         switch (this._posicion){
+             //escaleras
             case 2:
-                this._posicion += 10;
+                this._posicion += 36;
                 break
             case 7:
-                this._posicion += -10;
+                this._posicion += 7;
 
             break
 
             case 15:
-                this._posicion += -10;
+                this._posicion += 11;
 
             break
 
             case 21:
-                this._posicion += -10;
+                this._posicion += 21;
             break
 
             case 28:
-                this._posicion += -10;
+                this._posicion += 54;
             break
 
             case 36:
-                this._posicion += -10;
+                this._posicion += 8;
             break
 
             case 51:
-                this._posicion += -10;
+                this._posicion += 16;
             break
 
             case 78:
+                this._posicion += 28;
+
+            case 2:
+                this._posicion += 36;
+                break
+            case 7:
+                this._posicion += 7;
+
+            break
+
+            case 15:
+                this._posicion += 11;
+
+            break
+
+            case 21:
+                this._posicion += 21;
+            break
+
+            case 28:
+                this._posicion += 54;
+            break
+
+            case 36:
+                this._posicion += 8;
+            break
+
+            case 51:
+                this._posicion += 16;
+            break
+
+            case 78:
+                this._posicion += 28;
+           case 2:
+                this._posicion += 36;
+                break
+            case 7:
+                this._posicion += 7;
+
+            break
+
+            case 15:
+                this._posicion += 11;
+
+            break
+
+            case 21:
+                this._posicion += 21;
+            break
+
+            case 28:
+                this._posicion += 54;
+            break
+
+            case 36:
+                this._posicion += 8;
+            break
+
+            case 51:
+                this._posicion += 16;
+            break
+
+            case 78:
+                this._posicion += 28;
+            break
+            //serpientes
+            case 16:
                 this._posicion += -10;
+                break
+            case 49:
+                this._posicion += -38;
+
+            break
+
+            case 62:
+                this._posicion += -43;
+
+            break
+
+            case 74:
+                this._posicion += -21;
+            break
+
+            case 89:
+                this._posicion += -21;
+            break
+
+            case 92:
+                this._posicion += -4;
+            break
+
+            case 95:
+                this._posicion += -20;
+            break
+
+            case 99:
+                this._posicion += -19;
+            break                 
         }
     }
 }
@@ -67,37 +167,135 @@ class Jugador2 extends Jugadores {
         this._posicion += n;
 
         switch (this._posicion){
+            //escaleras
             case 2:
-                this._posicion += 10;
+                this._posicion += 36;
                 break
             case 7:
-                this._posicion += 10;
+                this._posicion += 7;
 
             break
 
             case 15:
-                this._posicion += -10;
+                this._posicion += 11;
 
             break
 
             case 21:
-                this._posicion += 10;
+                this._posicion += 21;
             break
 
             case 28:
-                this._posicion += 10;
+                this._posicion += 54;
             break
 
             case 36:
-                this._posicion += -10;
+                this._posicion += 8;
             break
 
             case 51:
-                this._posicion += 10;
+                this._posicion += 16;
             break
 
             case 78:
-                this._posicion += 10;
+                this._posicion += 28;
+
+            case 2:
+                this._posicion += 36;
+                break
+            case 7:
+                this._posicion += 7;
+
+            break
+
+            case 15:
+                this._posicion += 11;
+
+            break
+
+            case 21:
+                this._posicion += 21;
+            break
+
+            case 28:
+                this._posicion += 54;
+            break
+
+            case 36:
+                this._posicion += 8;
+            break
+
+            case 51:
+                this._posicion += 16;
+            break
+
+            case 78:
+                this._posicion += 28;
+         case 2:
+                this._posicion += 36;
+                break
+            case 7:
+                this._posicion += 7;
+
+            break
+
+            case 15:
+                this._posicion += 11;
+
+            break
+
+            case 21:
+                this._posicion += 21;
+            break
+
+            case 28:
+                this._posicion += 54;
+            break
+
+            case 36:
+                this._posicion += 8;
+            break
+
+            case 51:
+                this._posicion += 16;
+            break
+
+            case 78:
+                this._posicion += 28;
+            break
+            //serpientes
+            case 16:
+                this._posicion += -10;
+                break
+            case 49:
+                this._posicion += -38;
+
+            break
+
+            case 62:
+                this._posicion += -43;
+
+            break
+
+            case 74:
+                this._posicion += -21;
+            break
+
+            case 89:
+                this._posicion += -21;
+            break
+
+            case 92:
+                this._posicion += -4;
+            break
+
+            case 95:
+                this._posicion += -20;
+            break
+
+            case 99:
+                this._posicion += -19;
+            break              
         }
     }
 }
