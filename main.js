a3217b713988620a179f433e62cf6674badecfe8
pakkofrank <jpalma@ucol.mx>
2019-09-19 00:30:07 -0500

    todo lo que pude hacer GG

--- main.js
+++ main.js
@@ -30,131 +30,88 @@ class Jugador1 extends Jugadores {
         switch (this._posicion){
              //escaleras
             case 2:
+                console.log(this._posicion)
                 this._posicion += 36;
                 break
             case 7:
+                console.log(this._posicion)
+
                 this._posicion += 7;
 
             break
 
             case 15:
+                console.log(this._posicion)
+
                 this._posicion += 11;
 
             break
 
             case 21:
+                console.log(this._posicion)
+
                 this._posicion += 21;
             break
 
             case 28:
+                console.log(this._posicion)
                 this._posicion += 54;
             break
 
             case 36:
+                console.log(this._posicion)
                 this._posicion += 8;
             break
 
             case 51:
+                console.log(this._posicion)
                 this._posicion += 16;
             break
 
             case 78:
+                console.log(this._posicion)
                 this._posicion += 28;
+            break  
 
-            case 2:
-                this._posicion += 36;
-                break
-            case 7:
-                this._posicion += 7;
-
-            break
-
-            case 15:
-                this._posicion += 11;
-
-            break
-
-            case 21:
-                this._posicion += 21;
-            break
-
-            case 28:
-                this._posicion += 54;
-            break
-
-            case 36:
-                this._posicion += 8;
-            break
-
-            case 51:
-                this._posicion += 16;
-            break
-
-            case 78:
-                this._posicion += 28;
-           case 2:
-                this._posicion += 36;
-                break
-            case 7:
-                this._posicion += 7;
-
-            break
-
-            case 15:
-                this._posicion += 11;
-
-            break
-
-            case 21:
-                this._posicion += 21;
-            break
-
-            case 28:
-                this._posicion += 54;
-            break
-
-            case 36:
-                this._posicion += 8;
-            break
-
-            case 51:
-                this._posicion += 16;
-            break
-
-            case 78:
-                this._posicion += 28;
-            break
             //serpientes
             case 16:
+                console.log(this._posicion)
                 this._posicion += -10;
                 break
             case 49:
+                console.log(this._posicion)
                 this._posicion += -38;
 
             break
 
             case 62:
+                console.log(this._posicion)
                 this._posicion += -43;
 
             break
 
             case 74:
+                console.log(this._posicion)
                 this._posicion += -21;
             break
 
             case 89:
+                console.log(this._posicion)
                 this._posicion += -21;
             break
 
             case 92:
+                console.log(this._posicion)
                 this._posicion += -4;
             break
 
             case 95:
+                console.log(this._posicion)
                 this._posicion += -20;
             break
 
             case 99:
+                console.log(this._posicion)
                 this._posicion += -19;
             break                 
         }
@@ -168,135 +125,92 @@ class Jugador2 extends Jugadores {
 
         switch (this._posicion){
             //escaleras
-            case 2:
-                this._posicion += 36;
-                break
-            case 7:
-                this._posicion += 7;
+           case 2:
+               console.log(this._posicion)
+               this._posicion += 36;
+               break
+           case 7:
+               console.log(this._posicion)
 
-            break
+               this._posicion += 7;
 
-            case 15:
-                this._posicion += 11;
+           break
 
-            break
+           case 15:
+               console.log(this._posicion)
 
-            case 21:
-                this._posicion += 21;
-            break
+               this._posicion += 11;
 
-            case 28:
-                this._posicion += 54;
-            break
+           break
 
-            case 36:
-                this._posicion += 8;
-            break
+           case 21:
+               console.log(this._posicion)
 
-            case 51:
-                this._posicion += 16;
-            break
+               this._posicion += 21;
+           break
 
-            case 78:
-                this._posicion += 28;
+           case 28:
+               console.log(this._posicion)
+               this._posicion += 54;
+           break
 
-            case 2:
-                this._posicion += 36;
-                break
-            case 7:
-                this._posicion += 7;
+           case 36:
+               console.log(this._posicion)
+               this._posicion += 8;
+           break
 
-            break
+           case 51:
+               console.log(this._posicion)
+               this._posicion += 16;
+           break
 
-            case 15:
-                this._posicion += 11;
+           case 78:
+               console.log(this._posicion)
+               this._posicion += 28;
+            break 
 
-            break
+           //serpientes
+           case 16:
+               console.log(this._posicion)
+               this._posicion += -10;
+               break
+           case 49:
+               console.log(this._posicion)
+               this._posicion += -38;
 
-            case 21:
-                this._posicion += 21;
-            break
+           break
 
-            case 28:
-                this._posicion += 54;
-            break
+           case 62:
+               console.log(this._posicion)
+               this._posicion += -43;
 
-            case 36:
-                this._posicion += 8;
-            break
+           break
 
-            case 51:
-                this._posicion += 16;
-            break
+           case 74:
+               console.log(this._posicion)
+               this._posicion += -21;
+           break
 
-            case 78:
-                this._posicion += 28;
-             case 2:
-                this._posicion += 36;
-                break
-            case 7:
-                this._posicion += 7;
+           case 89:
+               console.log(this._posicion)
+               this._posicion += -21;
+           break
 
-            break
+           case 92:
+               console.log(this._posicion)
+               this._posicion += -4;
+           break
 
-            case 15:
-                this._posicion += 11;
+           case 95:
+               console.log(this._posicion)
+               this._posicion += -20;
+           break
 
-            break
-
-            case 21:
-                this._posicion += 21;
-            break
-
-            case 28:
-                this._posicion += 54;
-            break
-
-            case 36:
-                this._posicion += 8;
-            break
-
-            case 51:
-                this._posicion += 16;
-            break
-
-            case 78:
-                this._posicion += 28;
-            break
-            //serpientes
-            case 16:
-                this._posicion += -10;
-                break
-            case 49:
-                this._posicion += -38;
-
-            break
-
-            case 62:
-                this._posicion += -43;
-
-            break
-
-            case 74:
-                this._posicion += -21;
-            break
-
-            case 89:
-                this._posicion += -21;
-            break
-
-            case 92:
-                this._posicion += -4;
-            break
-
-            case 95:
-                this._posicion += -20;
-            break
-
-            case 99:
-                this._posicion += -19;
-            break              
-        }
+           case 99:
+               console.log(this._posicion)
+               this._posicion += -19;
+           break                 
+       }
     }
 }
 
@@ -309,16 +223,19 @@ while(j1.posicion<100 && j2.posicion<100){
     j2.avanzar();
     console.log(j1.posicion,j2.posicion);
 
-    document.querySelector("#resultado").innerHTML += j1.posicion+"<br>"+"jugador1"+"     ";
-    document.querySelector("#resultado").innerHTML += j2.posicion+"<br>"+"jugador2"+"     ";
+    document.querySelector("#resultado").innerHTML += "jugador1"+" cayo en la casilla  "+j1.posicion+"<br>";
+    document.querySelector("#resultado").innerHTML += "jugador2"+" cayo en la casilla  "+j2.posicion+"<br>";
 }
 
 if (j1.posicion>=100 && j2.posicion>=100){
     console.log("empate");
+    document.querySelector("#resultado3").innerHTML = "empate";
 }else if(j1.posicion>=100){
     console.log("jugador 1 gano");
+    document.querySelector("#resultado3").innerHTML = "gano el jugador 1";
 }else{
     console.log("jugador 2 gano");
+    document.querySelector("#resultado3").innerHTML = "gano el jugador 2";
 }
 
 
